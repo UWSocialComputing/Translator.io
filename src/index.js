38ac98bd1dbdd2b1aad3d8e2aac810c4fc09104f
RickySin11 <69612398+SinghxRaj@users.noreply.github.com>
2023-05-14 15:15:23 -0700

    Only register commands in 'actual' directory

--- src/index.js
+++ src/index.js
@@ -29,21 +29,18 @@ const client = new Client({
 });
 
 client.commands = new Collection();
-const foldersPath = path.join(__dirname, 'commands');
-const commandFolders = fs.readdirSync(foldersPath);
+const filesPath = path.join(__dirname, 'commands/actual');
+const files = fs.readdirSync(filesPath);
 
-for (const folder of commandFolders) {
-	const commandsPath = path.join(foldersPath, folder);
-	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-	for (const file of commandFiles) {
-		const filePath = path.join(commandsPath, file);
-		const command = require(filePath);
-		if ('data' in command && 'execute' in command) {
-			client.commands.set(command.data.name, command);
-		}
-		else {
-			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-		}
+for (const file of files) {
+	console.log(file);
+	const filePath = path.join(filesPath, file);
+	const command = require(filePath);
+	if ('data' in command && 'execute' in command) {
+		client.commands.set(command.data.name, command);
+	}
+	else {
+		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
 	}
 }
 
