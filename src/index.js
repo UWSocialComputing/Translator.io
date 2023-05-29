5eef99f7f7f1ec294d70ee6ad95c739c3d975a57
RickySin11 <69612398+SinghxRaj@users.noreply.github.com>
2023-05-29 12:50:08 -0700

    Send DM to members on auto translate

--- src/index.js
+++ src/index.js
@@ -4,7 +4,11 @@ const log4js = require('log4js');
 log4js.configure({
 	appenders: {
 		out: { type: 'console' },
-		app: { type: 'file', filename: 'logs/index.log', category: 'index' },
+		app: {
+			type: 'file',
+			filename: 'logs/index.log',
+			category: 'index',
+		},
 	},
 	categories: {
 		default: { appenders: ['out', 'app'], level: 'debug' },
@@ -24,16 +28,16 @@ const deepl = require('deepl-node');
 const translator = new deepl.Translator(apiKey);
 
 // sequilize elements
-const { User, Channel, Server, Searchable } = require('./models');
+const { User, Channel, Server } = require('./models');
 
 const client = new Client({
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.MessageContent,
-        GatewayIntentBits.GuildMembers,
-        GatewayIntentBits.GuildMessages,
-    ],
-    partials: [Partials.Message, Partials.Channel, Partials.Reaction],
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.MessageContent,
+		GatewayIntentBits.GuildMembers,
+		GatewayIntentBits.GuildMessages,
+	],
+	partials: [Partials.Message, Partials.Channel, Partials.Reaction],
 });
 
 client.commands = new Collection();
@@ -41,42 +45,41 @@ const filesPath = path.join(__dirname, 'commands/actual');
 const files = fs.readdirSync(filesPath);
 
 for (const file of files) {
-    console.log(file);
-    const filePath = path.join(filesPath, file);
-    const command = require(filePath);
-    if ('data' in command && 'execute' in command) {
-        client.commands.set(command.data.name, command);
-    }
-    else {
-        console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-    }
+	console.log(file);
+	const filePath = path.join(filesPath, file);
+	const command = require(filePath);
+	if ('data' in command && 'execute' in command) {
+		client.commands.set(command.data.name, command);
+	}
+	else {
+		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+	}
 }
 
 client.once(Events.ClientReady, async () => {
-    await Server.sync();
-    await Channel.sync();
-    await User.sync();
-    await Searchable.sync();
-    console.log('Bot is ready!');
+	await Server.sync();
+	await Channel.sync();
+	await User.sync();
+	console.log('Bot is ready!');
 });
 
 // Respond to \ commands
 client.on(Events.InteractionCreate, async interaction => {
-    if (!interaction.isChatInputCommand()) return;
-    const command = client.commands.get(interaction.commandName);
-    if (!command) return;
-    try {
-        await command.execute(interaction);
-    }
-    catch (error) {
-        console.error(error);
-        if (interaction.replied || interaction.deferred) {
-            await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
-        }
-        else {
-            await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-        }
-    }
+	if (!interaction.isChatInputCommand()) return;
+	const command = client.commands.get(interaction.commandName);
+	if (!command) return;
+	try {
+		await command.execute(interaction);
+	}
+	catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
+		else {
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
+	}
 });
 
 // Auto-translates messages in channels it is enabled in
@@ -86,15 +89,24 @@ client.on(Events.MessageCreate, async msg => {
 	const channelId = msg.channelId;
 
 	const channel = await client.channels.fetch(channelId);
-	
 	const members = channel.members;
-	
 
-	for (const member in members) {
+	// logger.info('Entering Auto-Translate ...');
+	// logger.info(`Size of members: ${members.size}`);
+	for (const member of members.values()) {
+		logger.info(`member name ${member.user.username}`);
+		if (member.user.username == 'Translator.io') {
+			// logger.info('Skipping Translator.io ...');
+			continue;
+		}
+		if (member.id == msg.member.id) {
+			// logger.info('Skipping original sender ...');
+			continue;
+		}
+		// logger.info(`Translating for ${member.id}`);
 		await sendTranslationToUser(msg, serverId, channelId, member.id);
 	}
 
-	
 });
 
 async function sendTranslationToUser(msg, serverId, channelId, targetUserId) {
@@ -127,8 +139,8 @@ async function sendTranslationToUser(msg, serverId, channelId, targetUserId) {
 			console.log(error);
 			output = 'Sorry, there is a problem with our translator.';
 		}
+		// logger.info(`Sending translation to ${targetUserId}, output : ${output}`);
 		await client.users.send(targetUserId, output);
-		// await msg.reply({ content: output, allowedMentions: { repliedUser: false } });
 	}
 }
 
